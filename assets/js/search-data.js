12e676d03b7992e44e47b6f835386a7804ca1b6b
KIKI007 <qiqiustc@gmail.com>
2026-05-30 12:26:24 +0000

    Deploying to gh-pages from @ KIKI007/KIKI007.github.io@6974cf6296d1f2f4afc23abe75d2178aac1c7e38 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,18 +405,12 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-we-have-opening-for-2027-fall-in-graphics-and-embodied-ai",
+          title: 'We have opening for 2027 Fall in Graphics and Embodied AI.',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+              window.location.href = "/news/announcement_1/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
